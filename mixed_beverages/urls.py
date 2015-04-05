2d7beed8feadd1c75ac3936117bc4438ccc9395f
crccheck <c@crccheck.com>
2015-04-05 15:46:10 -0500

    only enable the django admin in debug

--- mixed_beverages/urls.py
+++ mixed_beverages/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -10,5 +11,9 @@ urlpatterns = [
     url(r'^geo/', include('mixed_beverages.apps.lazy_geo.urls',
         namespace='lazy_geo')),
     url(r'^api/', include(router.urls)),
-    url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^admin/', include(admin.site.urls)),
+    ]
