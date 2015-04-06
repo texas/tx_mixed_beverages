b748355a892c0934fb8dc05b16ae751325b56772
crccheck <c@crccheck.com>
2015-04-05 23:37:49 -0500

    link to about page

--- mixed_beverages/static_src/js/app.js
+++ mixed_beverages/static_src/js/app.js
@@ -82,7 +82,11 @@ if (document.getElementById('map')) {
 
   L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.jpg', {
     maxZoom: 18,
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+    attribution: `Map tiles by <a href="http://stamen.com">Stamen Design</a>,
+                  under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.
+                  Data by <a href="http://openstreetmap.org">OpenStreetMap</a>,
+                  under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.
+                  <a href="/about/">About</a> this site.`
   }).addTo(map);
 
 
