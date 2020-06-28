0c3a27db029b715d205341e135645e3ff65ee1e9
crccheck <c@crccheck.com>
2020-06-28 00:35:07 -0500

    get runserver to run server

--- mixed_beverages/urls.py
+++ mixed_beverages/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 from django.views.generic import TemplateView
 
 from mixed_beverages.apps.receipts.api import router
@@ -16,9 +17,5 @@ urlpatterns = [
     ),
     url(r"^geo/", include("mixed_beverages.apps.lazy_geo.urls", namespace="lazy_geo")),
     url(r"^api/", include(router.urls)),
+    path("admin/", admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        url(r"^admin/", include(admin.site.urls)),
-    ]
--- mixed_beverages/wsgi.py
+++ mixed_beverages/wsgi.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mixed_beverages.settings")
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
