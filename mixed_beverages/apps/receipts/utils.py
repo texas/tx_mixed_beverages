b2786a3e0c040d2abc71adfd1cba160bee03bb0c
crccheck <c@crccheck.com>
2020-07-03 03:23:46 -0500

    calculate average in the database

--- mixed_beverages/apps/receipts/utils.py
+++ mixed_beverages/apps/receipts/utils.py
@@ -2,7 +2,7 @@ import csv
 import datetime
 import os
 
-from django.db.models import Count
+from django.db.models import Count, Avg
 from tqdm import tqdm
 
 from .models import Receipt, Business, Location
@@ -31,30 +31,25 @@ def assign_businesses(show_progress=False):
 
 def set_location_data(show_progress=False):
     """
-    Denormalizes data into the `Location` model.
+    Denormalizes receipt data into the `Location` model.
+
+    Location name is set in the "slurp" step.
 
     timing: real    2m50.342s
     """
     latest_receipt_date = Receipt.objects.latest("date").date
 
+    print(f"Set monthly receipts for the past 4 months, up to {latest_receipt_date}")
     queryset = Location.objects.all()
     # Good enough, go back 4 * 31 days to get 4 months
     cutoff_date = latest_receipt_date - datetime.timedelta(days=4 * 31)
     for x in tqdm(queryset, disable=not show_progress):
-        latest_receipts = list(x.receipts.order_by("-date")[:4])
-        latest_receipt = latest_receipts[0]
-        recent_receipts = list(filter(lambda x: x.date > cutoff_date, latest_receipts))
-        if not recent_receipts:
-            # clear old data
-            x.data = {
-                "name": str(latest_receipt.name),
-                "avg_tax": "0",
-            }
-            x.save(update_fields=("data",))
-            continue
-        avg_tax = sum(x.total for x in recent_receipts) / len(recent_receipts)
+        receipt_stats = x.receipts.filter(total__gt=0, date__gt=cutoff_date).aggregate(
+            Avg("total")
+        )
         x.data = {
-            "name": str(latest_receipt.name),
-            "avg_tax": "{:.2f}".format(avg_tax),
+            "avg_total": "{:.2f}".format(receipt_stats["total__avg"]),
+            **x.data,  # Preserve "name"
         }
-        x.save(update_fields=("data",))
+        print(x.data)
+        # x.save(update_fields=("data",))
