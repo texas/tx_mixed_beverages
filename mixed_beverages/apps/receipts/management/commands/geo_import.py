f12d751baa79e0651084299aed67992e7e5b1aa6
Chris Chang <c@crccheck.com>
2020-07-04 01:48:13 -0500

    feat: Geocode tweaks (#39)
    * add a way to limit locations exported
    
    * only export un-geocoded
    
    import via pk
    
    store some sort of coordinate quality
    
    oops get out of here tiger
    
    * ignore pk field if desired

--- mixed_beverages/apps/receipts/management/commands/geo_import.py
+++ mixed_beverages/apps/receipts/management/commands/geo_import.py
@@ -9,13 +9,30 @@ from tqdm import tqdm
 from mixed_beverages.apps.receipts.models import Location
 
 
+def get_coordinate_quality(accuracy: str) -> str:
+    # https://www.geocod.io/guides/accuracy-types-scores/
+    if accuracy == "1":
+        return "00"
+
+    accuracy_value = float(accuracy)
+    if accuracy_value > 0.8:
+        return "01"
+
+    return "98"
+
+
 class Command(BaseCommand):
-    help = "Import batch geocoding results from Geocodio"
+    help = (
+        "Import batch geocoding results from Geocodio. Overwrites existing coordinates."
+    )
 
     def add_arguments(self, parser):
         parser.add_argument("csv")
+        parser.add_argument(
+            "--ignore-pk", action="store_true", help='ignore "pk" field of csv'
+        )
 
-    def handle(self, csv, *args, **options):
+    def handle(self, csv: str, ignore_pk: bool, *args, **options):
         assert os.path.isfile(csv)
 
         with open(csv, "r") as fh:
@@ -23,16 +40,19 @@ class Command(BaseCommand):
             fh.seek(0)
             reader = DictReader(fh)
             for row in tqdm(reader, total=row_count - 1):
-                # TODO can I add `pk` to the row to simplify this query?
-                location = Location.objects.get(
-                    street_address=row["street_address"],
-                    city=row["city"],
-                    state=row["state"],
-                    zip=row["zip"],
-                )
+                if ignore_pk:
+                    location = Location.objects.get(
+                        street_address=row["street_address"],
+                        city=row["city"],
+                        state=row["state"],
+                        zip=row["zip"],
+                    )
+                else:
+                    location = Location.objects.get(pk=row["pk"])
                 location.coordinate = Point(
                     x=float(row["Longitude"]), y=float(row["Latitude"])
                 )
-                # TODO turn row['Accuracy Score'] into location.coordinate_quality
+                location.coordinate_quality = get_coordinate_quality(
+                    row["Accuracy Score"]
+                )
                 location.save()
-                # print(location, row)
