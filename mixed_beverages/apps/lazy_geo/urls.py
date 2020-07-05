3d11c103e60a13fbaf4a9908101ad3e078a9cd05
crccheck <c@crccheck.com>
2020-07-04 22:48:56 -0500

    delete fixit server side

--- mixed_beverages/apps/lazy_geo/urls.py
+++ mixed_beverages/apps/lazy_geo/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from django.views.decorators.cache import cache_control
-from django.views.generic import TemplateView
 
 from . import views
 
@@ -14,16 +13,5 @@ urlpatterns = [
         r"^data.geojson$",
         cache_control(max_age=ONE_WEEK, public=True)(views.MarkerList.as_view()),
         name="geo",
-    ),
-    url(r"^fix/(?P<pk>\d+)/$", views.FixDetail.as_view(), name="fixit"),
-    url(
-        r"^correction/(?P<pk>\d+)/$",
-        views.CorrectionDetail.as_view(),
-        name="correction-detail",
-    ),
-    url(
-        r"^thanks/$",
-        TemplateView.as_view(template_name="lazy_geo/thanks.html"),
-        name="thanks",
     ),
 ]
