03822556b8b2bed57757cf8c0fb05b1a1978f447
crccheck <c@crccheck.com>
2020-07-04 22:15:47 -0500

    ahhh, that's how to turn off semicolons

--- mixed_beverages/static_src/js/app.js
+++ mixed_beverages/static_src/js/app.js
@@ -1,15 +1,15 @@
-import L from "leaflet";
+import L from "leaflet"
 
-import * as bigmap from "./bigmap";
+import * as bigmap from "./bigmap"
 // import * as fixit from "./fixitmap";
 
 // need to manually specify this
-L.Icon.Default.imagePath = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/images";
+L.Icon.Default.imagePath = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/images"
 
 // BEGIN
 // var bodyClass = document.body.className;
 // if (bodyClass.indexOf("bigmap") !== -1) {
-bigmap.render();
+bigmap.render()
 // } else if (bodyClass.indexOf("fixit") !== -1) {
 //   fixit.render();
 // }
--- mixed_beverages/static_src/js/bigmap.js
+++ mixed_beverages/static_src/js/bigmap.js
@@ -1,21 +1,21 @@
 /* global URLS: false */
-import L from "leaflet";
-import "leaflet.markercluster";
-import "leaflet-hash";
-import "./ui/Control.GeoZoom";
-import $ from "jquery";
-import _ from "lodash";
-import * as d3 from "d3";
-import Cookies from "cookies-js";
-window.d3 = d3; // DEBUG
+import L from "leaflet"
+import "leaflet.markercluster"
+import "leaflet-hash"
+import "./ui/Control.GeoZoom"
+import $ from "jquery"
+import _ from "lodash"
+import * as d3 from "d3"
+import Cookies from "cookies-js"
+window.d3 = d3 // DEBUG
 
-import { DECLUSTER_ZOOM } from "./settings";
-import { showLocationPopup } from "./marker_utils";
-import { taxColorScale } from "./utils";
-import Nav from "./ui/Nav";
-import Legend from "./ui/Legend";
+import { DECLUSTER_ZOOM } from "./settings"
+import { showLocationPopup } from "./marker_utils"
+import { taxColorScale } from "./utils"
+import Nav from "./ui/Nav"
+import Legend from "./ui/Legend"
 
-var map, nav;
+var map, nav
 
 var markerStyle = function (feature) {
   var style = {
@@ -23,48 +23,48 @@ var markerStyle = function (feature) {
     opacity: 0.9,
     radius: 7,
     weight: 1,
-  };
-  var tax = parseInt(feature.properties.data.avg_tax, 10); // actually a float, but we don't care about cents
-  if (tax === 0) {
-    style.color = taxColorScale(tax);
-    style.radius = 4;
-  } else {
-    style.fillColor = taxColorScale(tax);
-    style.color = d3.rgb(style.fillColor).darker(1);
   }
-  return style;
-};
+  var tax = parseInt(feature.properties.data.avg_tax, 10) // actually a float, but we don't care about cents
+  if (tax === 0) {
+    style.color = taxColorScale(tax)
+    style.radius = 4
+  } else {
+    style.fillColor = taxColorScale(tax)
+    style.color = d3.rgb(style.fillColor).darker(1)
+  }
+  return style
+}
 
 function _getJSON(data) {
   const markers = new L.MarkerClusterGroup({
     disableClusteringAtZoom: DECLUSTER_ZOOM,
     maxClusterRadius: 50,
-  });
+  })
   L.geoJson(data, {
     pointToLayer: (feature, latlng) => L.circleMarker(latlng, markerStyle(feature)),
-  }).addTo(markers);
-  nav.saveMarkers(markers);
-  markers.addTo(map);
+  }).addTo(markers)
+  nav.saveMarkers(markers)
+  markers.addTo(map)
   markers.on("click", function (evt) {
     // console.log("marker", evt.layer, this); // DEBUG
-    showLocationPopup(evt.layer);
-  });
+    showLocationPopup(evt.layer)
+  })
   var updateNav = function () {
     var data = {
       value: 0,
       markers: [],
-    };
-    var bounds = map.getBounds();
+    }
+    var bounds = map.getBounds()
     markers.eachLayer(function (marker) {
       if (bounds.contains(marker.getLatLng())) {
-        data.markers.push(marker);
-        data.value += parseFloat(marker.feature.properties.data.avg_tax || 0);
+        data.markers.push(marker)
+        data.value += parseFloat(marker.feature.properties.data.avg_tax || 0)
       }
-    });
-    nav.showStatsFor(data);
-  };
-  updateNav(); // initial hit
-  map.on("move", _.debounce(updateNav, 500));
+    })
+    nav.showStatsFor(data)
+  }
+  updateNav() // initial hit
+  map.on("move", _.debounce(updateNav, 500))
 }
 
 function firstVisit() {
@@ -88,7 +88,7 @@ function firstVisit() {
     </p>
     </div>`
     )
-    .openOn(map);
+    .openOn(map)
 }
 
 export function render() {
@@ -97,18 +97,18 @@ export function render() {
     center: [30.2655, -97.7426],
     zoom: DECLUSTER_ZOOM,
     zoomControl: false,
-  });
+  })
   if (Cookies.enabled && !Cookies.get("returning")) {
-    Cookies.set("returning", "1", { expires: 86400 * 30 * 3 });
-    firstVisit();
+    Cookies.set("returning", "1", { expires: 86400 * 30 * 3 })
+    firstVisit()
   }
-  var legend = new Legend();
-  nav = new Nav(map, showLocationPopup);
-  map.addControl(new L.Control.GeoZoom());
-  map.addControl(legend.render());
-  map.addControl(nav.render());
-  window.map = map; // DEBUG
-  new L.hash(map); // eslint-disable-line
+  var legend = new Legend()
+  nav = new Nav(map, showLocationPopup)
+  map.addControl(new L.Control.GeoZoom())
+  map.addControl(legend.render())
+  map.addControl(nav.render())
+  window.map = map // DEBUG
+  new L.hash(map) // eslint-disable-line
 
   L.tileLayer("https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png", {
     maxZoom: 18,
@@ -117,7 +117,7 @@ export function render() {
                   Data by <a href="https://openstreetmap.org">OpenStreetMap</a>,
                   under <a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.
                   <a href="/about/">About</a> this site.`,
-  }).addTo(map);
+  }).addTo(map)
 
-  $.getJSON(URLS.geojson, _getJSON);
+  $.getJSON(URLS.geojson, _getJSON)
 }
