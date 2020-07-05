096885bb27f2a9b971dd390ec0d3d9f45d34de3a
crccheck <c@crccheck.com>
2020-07-05 02:44:29 -0500

    update data importing

--- mixed_beverages/apps/receipts/management/commands/slurp.py
+++ mixed_beverages/apps/receipts/management/commands/slurp.py
@@ -29,7 +29,7 @@ def Location_get(street_address, city, state, zip, name):
         city=city,
         state=state,
         zip=zip,
-        defaults=dict(data={"name": name}),
+        defaults=dict(name=name),
     )
     return location
 
@@ -57,11 +57,11 @@ class Command(BaseCommand):
                 )
                 receipt, created = obj_update_or_create(
                     Receipt,
-                    tax_number=row["Taxpayer Number"],
+                    tabc_permit=row["TABC Permit Number"],
                     date=date_fmt(row["Obligation End Date"]),
                     defaults=dict(
-                        name=row["Taxpayer Name"],
-                        tabc_permit=row["TABC Permit Number"],
+                        taxpayer_name=row["Taxpayer Name"],
+                        tax_number=row["Taxpayer Number"],
                         liquor=row["Liquor Receipts"],
                         wine=row["Wine Receipts"],
                         beer=row["Beer Receipts"],
