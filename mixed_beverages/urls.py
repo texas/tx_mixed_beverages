229dccfe48d3e1d5483f46d04e7d60e530e1bc8c
crccheck <c@crccheck.com>
2020-07-06 23:42:19 -0500

    delete confusing urls.py

--- mixed_beverages/urls.py
+++ mixed_beverages/urls.py
@@ -2,9 +2,15 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.cache import cache_control
 from django.views.generic import TemplateView
 
 from mixed_beverages.apps.receipts.api import router
+
+from .apps.lazy_geo import views
+
+ONE_DAY = 86400
+ONE_WEEK = ONE_DAY * 7
 
 
 urlpatterns = [
@@ -15,7 +21,11 @@ urlpatterns = [
     url(
         r"", include("mixed_beverages.apps.receipts.urls", namespace="mixed_beverages")
     ),
-    url(r"^geo/", include("mixed_beverages.apps.lazy_geo.urls", namespace="lazy_geo")),
+    path(
+        "geo/data.geojson",
+        cache_control(max_age=ONE_WEEK, public=True)(views.MarkerList.as_view()),
+        name="geo_list",
+    ),
     url(r"^api/", include(router.urls)),
     path("admin/", admin.site.urls),
 ]
