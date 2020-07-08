50257d49c81da0f07d44aea1b010c2193b6f5054
crccheck <c@crccheck.com>
2020-07-07 23:40:55 -0500

    more style updates

--- mixed_beverages/static_src/js/ui/legendFactory.js
+++ mixed_beverages/static_src/js/ui/legendFactory.js
@@ -10,15 +10,15 @@ export default function legendFactory() {
       position: "bottomleft",
     },
     onAdd: function () {
-      var $container = $('<div class="legend leaflet-bar"/>')
-      var $list = $("<dl>").appendTo($container)
+      const $container = $('<div class="legend leaflet-bar"/>')
+      const $list = $("<dl>").appendTo($container)
       $.each(taxColorScale.domain(), function (idx, level) {
-        var fill = taxColorScale(level)
-        var border = d3.rgb(fill).darker(1)
+        const fill = taxColorScale(level)
+        const border = d3.rgb(fill).darker(1)
         $list.append(`<dt>
           <span style="background: ${fill}; border: 1px solid ${border};">&nbsp;</span>
           </dt>`)
-        $list.append("<dd>" + thousands(level) + "</dd>")
+        $list.append(`<dd>${thousands(level)}</dd>`)
       })
       return $container[0]
     },
