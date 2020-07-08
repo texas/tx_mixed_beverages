433f9fed046df152e43714797b95a1948effbaf2
Chris Chang <c@crccheck.com>
2020-07-07 23:49:11 -0500

    refactor: modernize Legend syntax (#48)
    * style pass
    
    * yagni function instead of class
    
    * more style updates
    
    * use Object.values instead of jquery

---
+++ mixed_beverages/static_src/js/ui/legendFactory.js
@@ -0,0 +1,27 @@
+import L from "leaflet"
+import $ from "jquery"
+import * as d3 from "d3"
+
+import { thousands, taxColorScale } from "../utils"
+
+export default function legendFactory() {
+  const Legend = L.Control.extend({
+    options: {
+      position: "bottomleft",
+    },
+    onAdd() {
+      const $container = $('<div class="legend leaflet-bar"/>')
+      const $list = $("<dl>").appendTo($container)
+      Object.values(taxColorScale.domain()).forEach((level) => {
+        const fill = taxColorScale(level)
+        const border = d3.rgb(fill).darker(1)
+        $list.append(`<dt>
+          <span style="background: ${fill}; border: 1px solid ${border};">&nbsp;</span>
+          </dt>`)
+        $list.append(`<dd>${thousands(level)}</dd>`)
+      })
+      return $container[0]
+    },
+  })
+  return new Legend()
+}
