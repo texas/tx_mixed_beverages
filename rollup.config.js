b21a7ab92b60e6854c2474320571050c891858a4
crccheck <c@crccheck.com>
2020-07-10 23:52:53 -0500

    yagni

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import commonjs from "@rollup/plugin-commonjs"
 export default {
   input: "mixed_beverages/static_src/js/app.js",
   output: {
-    // directory: "mixed_beverages/static",
     file: "mixed_beverages/static/app.bundle.js",
     format: "esm",
   },
