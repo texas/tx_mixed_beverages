2880fc5a67eeea3901a9a8954ca4694e11515814
crccheck <c@crccheck.com>
2020-07-10 23:13:21 -0500

    d3-format

--- mixed_beverages/static_src/js/utils.js
+++ mixed_beverages/static_src/js/utils.js
@@ -1,9 +1,10 @@
+import { format as d3Format } from "d3-format"
 import * as d3 from "d3"
 import colorbrewer from "colorbrewer"
 import Bus from "@condenast/quick-bus"
 
-export const thousands = d3.format("$,.0f")
-export const distance = d3.format(",.1f")
+export const thousands = d3Format("$,.0f")
+export const distance = d3Format(",.1f")
 
 export const taxColorScale = d3
   .scaleLinear()
