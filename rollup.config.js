79dd50cdd24ed468d799ffe4bb6856d3b6864652
crccheck <c@crccheck.com>
2020-07-10 23:01:20 -0500

    add commonjs plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve"
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
 
 export default {
   input: "mixed_beverages/static_src/js/app.js",
   output: {
-    directory: "mixed_beverages/static",
+    // directory: "mixed_beverages/static",
+    file: "mixed_beverages/static/app.bundle.js",
     format: "es",
   },
-  plugins: [nodeResolve()],
+  plugins: [resolve(), commonjs()],
 }
