2c28d5a46976e2826e6419575898d344df73dc38
crccheck <c@crccheck.com>
2020-07-10 22:57:11 -0500

    add node-resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+
 export default {
   input: "mixed_beverages/static_src/js/app.js",
   output: {
     directory: "mixed_beverages/static",
     format: "es",
   },
+  plugins: [nodeResolve()],
 }
