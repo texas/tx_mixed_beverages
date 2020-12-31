fb281089f8a73875438f38325c05cc8f6dbfe129
crccheck <c@crccheck.com>
2020-12-30 23:55:54 -0600

    test pagination

--- mixed_beverages/apps/receipts/management/commands/slurp_api.py
+++ mixed_beverages/apps/receipts/management/commands/slurp_api.py
@@ -28,45 +28,53 @@ class Command(BaseCommand):
     help = "Import from Socrata API"
 
     def handle(self, *args, **options):
-        # TODO pagination
-        res = requests.get(
-            "https://data.texas.gov/resource/naix-2893.json",
-            # What the params do:
-            # https://dev.socrata.com/docs/queries/
-            params={"$order": "obligation_end_date_yyyymmdd desc"},
-        )
-        data = res.json()
-        print(f"Total rows: {len(data)}")
-        print(
-            f'Date range: {data[0]["obligation_end_date_yyyymmdd"]} - {data[-1]["obligation_end_date_yyyymmdd"]}'
-        )
-        created_count = 0
-        for row in data:
-            location = Location_get(
-                street_address=row["taxpayer_address"],
-                city=row["location_city"],
-                state=row["location_state"],
-                zip=row["location_zip"],
-                name=row["location_name"],
+        offset = 0
+        while True:
+            res = requests.get(
+                "https://data.texas.gov/resource/naix-2893.json",
+                # What the params do:
+                # https://dev.socrata.com/docs/queries/
+                params={
+                    "$order": "obligation_end_date_yyyymmdd desc",
+                    "$offset": offset,
+                },
             )
-            receipt, created = obj_update_or_create(
-                Receipt,
-                tabc_permit=row["tabc_permit_number"],
-                date=row["obligation_end_date_yyyymmdd"].split("T")[0],
-                defaults=dict(
-                    taxpayer_name=row["taxpayer_name"],
-                    tax_number=row["taxpayer_number"],
-                    liquor=row["liquor_receipts"],
-                    wine=row["wine_receipts"],
-                    beer=row["beer_receipts"],
-                    cover=row["cover_charge_receipts"],
-                    total=row["total_receipts"],
-                    location_name=row["location_name"],
-                    location_number=row["location_number"],
-                    county_code=row["location_county"],
-                    location=location,
-                ),
+            data = res.json()
+            print(f"Total rows: {len(data)}")
+            print(
+                f'Date range: {data[0]["obligation_end_date_yyyymmdd"]} - {data[-1]["obligation_end_date_yyyymmdd"]}'
             )
-            if created:
-                created_count + 1
-        print(f"Created   : {created_count}")
+            created_count = 0
+            for row in data:
+                location = Location_get(
+                    street_address=row["taxpayer_address"],
+                    city=row["location_city"],
+                    state=row["location_state"],
+                    zip=row["location_zip"],
+                    name=row["location_name"],
+                )
+                receipt, created = obj_update_or_create(
+                    Receipt,
+                    tabc_permit=row["tabc_permit_number"],
+                    date=row["obligation_end_date_yyyymmdd"].split("T")[0],
+                    defaults=dict(
+                        taxpayer_name=row["taxpayer_name"],
+                        tax_number=row["taxpayer_number"],
+                        liquor=row["liquor_receipts"],
+                        wine=row["wine_receipts"],
+                        beer=row["beer_receipts"],
+                        cover=row["cover_charge_receipts"],
+                        total=row["total_receipts"],
+                        location_name=row["location_name"],
+                        location_number=row["location_number"],
+                        county_code=row["location_county"],
+                        location=location,
+                    ),
+                )
+                if created:
+                    created_count + 1
+            print(f"Created   : {created_count}")
+            if created_count:
+                offset += len(data)
+            else:
+                break
