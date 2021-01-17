1b1d199b7df68efa7ad05cc1e38b9dc75b9c1339
crccheck <c@crccheck.com>
2021-01-17 16:57:30 -0600

    move data processing away from data fetching

--- mixed_beverages/apps/receipts/management/commands/slurp_api.py
+++ mixed_beverages/apps/receipts/management/commands/slurp_api.py
@@ -1,6 +1,6 @@
 import os.path
 from functools import lru_cache
-from typing import Tuple
+from typing import List, Tuple
 
 import requests
 
@@ -25,6 +25,41 @@ def Location_get(street_address, city, state, zip, name) -> Tuple[Location, bool
     )
 
 
+def import_data_from_api(data: List[dict]) -> int:
+    created_count = 0
+    for row in data:
+        location, location_created = Location_get(
+            street_address=row["taxpayer_address"],
+            city=row["location_city"],
+            state=row["location_state"],
+            zip=row["location_zip"],
+            name=row["location_name"],
+        )
+        if location_created:
+            print(f"Created Location: {location}")
+        receipt, created = obj_update_or_create(
+            Receipt,
+            tabc_permit=row["tabc_permit_number"],
+            date=row["obligation_end_date_yyyymmdd"].split("T")[0],
+            defaults=dict(
+                taxpayer_name=row["taxpayer_name"],
+                tax_number=row["taxpayer_number"],
+                liquor=row["liquor_receipts"],
+                wine=row["wine_receipts"],
+                beer=row["beer_receipts"],
+                cover=row["cover_charge_receipts"],
+                total=row["total_receipts"],
+                location_name=row["location_name"],
+                location_number=row["location_number"],
+                county_code=row["location_county"],
+                location=location,
+            ),
+        )
+        if created:
+            created_count + 1
+    return created_count
+
+
 class Command(BaseCommand):
     help = "Import from Socrata API"
 
@@ -46,37 +81,7 @@ class Command(BaseCommand):
             print(
                 f'Date range: {data[0]["obligation_end_date_yyyymmdd"]} - {data[-1]["obligation_end_date_yyyymmdd"]}'
             )
-            created_count = 0
-            for row in data:
-                location, location_created = Location_get(
-                    street_address=row["taxpayer_address"],
-                    city=row["location_city"],
-                    state=row["location_state"],
-                    zip=row["location_zip"],
-                    name=row["location_name"],
-                )
-                if location_created:
-                    print(f"Created Location: {location}")
-                receipt, created = obj_update_or_create(
-                    Receipt,
-                    tabc_permit=row["tabc_permit_number"],
-                    date=row["obligation_end_date_yyyymmdd"].split("T")[0],
-                    defaults=dict(
-                        taxpayer_name=row["taxpayer_name"],
-                        tax_number=row["taxpayer_number"],
-                        liquor=row["liquor_receipts"],
-                        wine=row["wine_receipts"],
-                        beer=row["beer_receipts"],
-                        cover=row["cover_charge_receipts"],
-                        total=row["total_receipts"],
-                        location_name=row["location_name"],
-                        location_number=row["location_number"],
-                        county_code=row["location_county"],
-                        location=location,
-                    ),
-                )
-                if created:
-                    created_count + 1
+            created_acount = import_data_from_api(data)
             print(f"Created   : {created_count}")
             if created_count:
                 offset += len(data)
