c58dfcd36a4bc5d55e9283217812882c22b5bd31
Chris Chang <c@crccheck.com>
2021-01-18 01:50:25 -0600

    feat: Fetch new data via API instead of processing a full CSV (#75)
    * add initial management command
    
    * create receipts
    
    * add better feedback
    
    * test pagination
    
    * give feedback for location creation
    
    * bump page size because we can't sort by upload date
    
    * move data processing away from data fetching
    
    * add mypy as dev req
    
    * add trivial test case
    
    * add failing test case
    
    * oooohhhhh
    
    * more feedback w/ location creation
    
    * default to 2500 geo exports to stay in free limit
    
    * greenkeeper
    
    * make sure import is idempotent

---
+++ mixed_beverages/apps/receipts/management/commands/slurp_api.py
@@ -0,0 +1,89 @@
+import os.path
+from functools import lru_cache
+from typing import List, Tuple
+
+import requests
+
+from django.core.management.base import BaseCommand
+from obj_update import obj_update_or_create
+from tqdm import tqdm
+
+from ...models import Location, Receipt
+
+
+@lru_cache(maxsize=512)
+def Location_get(street_address, city, state, zip, name) -> Tuple[Location, bool]:
+    """
+    Wrapper around Location.objects.get_or_create just to use lru_cache
+    """
+    return Location.objects.get_or_create(
+        street_address=street_address,
+        city=city,
+        state=state,
+        zip=zip,
+        defaults=dict(name=name),
+    )
+
+
+def import_data_from_api(data: List[dict]) -> int:
+    created_count = 0
+    for row in data:
+        location, location_created = Location_get(
+            street_address=row["taxpayer_address"],
+            city=row["location_city"],
+            state=row["location_state"],
+            zip=row["location_zip"],
+            name=row["location_name"],
+        )
+        if location_created:
+            print(f"Created Location: {location.pk} - {location}")
+        receipt, created = obj_update_or_create(
+            Receipt,
+            tabc_permit=row["tabc_permit_number"],
+            date=row["obligation_end_date_yyyymmdd"].split("T")[0],
+            defaults=dict(
+                taxpayer_name=row["taxpayer_name"],
+                tax_number=row["taxpayer_number"],
+                liquor=row["liquor_receipts"],
+                wine=row["wine_receipts"],
+                beer=row["beer_receipts"],
+                cover=row["cover_charge_receipts"],
+                total=row["total_receipts"],
+                location_name=row["location_name"],
+                location_number=row["location_number"],
+                county_code=row["location_county"],
+                location=location,
+            ),
+        )
+        if created:
+            created_count += 1
+    return created_count
+
+
+class Command(BaseCommand):
+    help = "Import from Socrata API"
+
+    def handle(self, *args, **options):
+        offset = 0
+        while True:
+            res = requests.get(
+                "https://data.texas.gov/resource/naix-2893.json",
+                # What the params do:
+                # https://dev.socrata.com/docs/queries/
+                params={
+                    "$order": "obligation_end_date_yyyymmdd desc",
+                    "$offset": offset,
+                    "$limit": 2000,  # Default: 1000
+                },
+            )
+            data = res.json()
+            print(f"Total rows: {len(data)}")
+            print(
+                f'Date range: {data[0]["obligation_end_date_yyyymmdd"]} - {data[-1]["obligation_end_date_yyyymmdd"]}'
+            )
+            created_count = import_data_from_api(data)
+            print(f"Created   : {created_count}")
+            if created_count:
+                offset += len(data)
+            else:
+                break
---
+++ mixed_beverages/apps/receipts/management/commands/test_slurp_api.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+
+from ...models import Location, Receipt
+from .slurp_api import Location_get, import_data_from_api
+
+# https://data.texas.gov/resource/naix-2893.json?$limit=1
+data = [
+    {
+        "taxpayer_number": "32047970895",
+        "taxpayer_name": "HONDURAS MAYA CAFE & BAR LLC",
+        "taxpayer_address": "8011 HAZEN ST",
+        "taxpayer_city": "HOUSTON",
+        "taxpayer_state": "TX",
+        "taxpayer_zip": "77036",
+        "taxpayer_county": "101",
+        "location_number": "1",
+        "location_name": "HONDURAS MAYA CAFE & BAR LLC",
+        "location_address": "5945 BELLAIRE BLVD STE B",
+        "location_city": "HOUSTON",
+        "location_state": "TX",
+        "location_zip": "77081",
+        "location_county": "101",
+        "inside_outside_city_limits_code_y_n": "Y",
+        "tabc_permit_number": "MB817033",
+        "responsibility_begin_date_yyyymmdd": "2012-08-16T00:00:00.000",
+        "responsibility_end_date_yyyymmdd": "2019-09-12T00:00:00.000",
+        "obligation_end_date_yyyymmdd": "2019-07-31T00:00:00.000",
+        "liquor_receipts": "0",
+        "wine_receipts": "0",
+        "beer_receipts": "0",
+        "cover_charge_receipts": "0",
+        "total_receipts": "0",
+    }
+]
+
+
+class SlurpApiTests(TestCase):
+    def setUp(self):
+        Location_get.cache_clear()
+
+    def test_trivial_nothing_happens_with_no_data(self):
+        created_count = import_data_from_api([])
+        self.assertEqual(created_count, 0)
+
+    def test_import_creates_row(self):
+        created_count = import_data_from_api(data)
+        self.assertEqual(created_count, 1)
+        self.assertEqual(Location.objects.count(), 1)
+        self.assertEqual(Receipt.objects.count(), 1)
+
+    def test_import_is_idempotent(self):
+        created_count = import_data_from_api(data)
+        self.assertEqual(created_count, 1)
+
+        created_count = import_data_from_api(data)
+        self.assertEqual(created_count, 0)
+        self.assertEqual(Location.objects.count(), 1)
+        self.assertEqual(Receipt.objects.count(), 1)
