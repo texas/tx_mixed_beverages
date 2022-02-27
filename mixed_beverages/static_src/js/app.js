3cb598087ad411d9c360022a8b3ec7280dda67f9
crccheck <c@crccheck.com>
2022-02-26 21:28:40 -0600

    found some more things I can upgrade

--- mixed_beverages/static_src/js/app.js
+++ mixed_beverages/static_src/js/app.js
@@ -3,6 +3,6 @@ import L from "leaflet"
 import * as bigmap from "./bigmap"
 
 // need to manually specify this
-L.Icon.Default.imagePath = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/images"
+L.Icon.Default.imagePath = "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images"
 
 bigmap.render()
